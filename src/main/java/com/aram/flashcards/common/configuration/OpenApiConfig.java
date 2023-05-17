fb9bc222ca262367a677b2f29b84d4a0d1ff5474
alex <raulalexo100@gmail.com>
2023-05-16 23:37:36 -0700

    Add @SecurityScheme annotation

--- src/main/java/com/aram/flashcards/common/configuration/OpenApiConfig.java
+++ src/main/java/com/aram/flashcards/common/configuration/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.aram.flashcards.common.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -15,6 +18,14 @@ import io.swagger.v3.oas.annotations.info.Info;
                 title = "Flashcards API",
                 version = "1.0"
         )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT token authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 class OpenApiConfig {
 }
